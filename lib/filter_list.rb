3caa65abf126862673c5a2cf6e8131fe4dec4df5
Katie McDonagh <mcdonagh.katie@outlook.com>
2021-01-18 14:47:22 +0000

    refactor

--- lib/filter_list.rb
+++ lib/filter_list.rb
@@ -1,10 +1,7 @@
 def filter_list(array)
   filtered_list = []
   array.each do |element|
-    if element.is_a?(Integer)
-       filtered_list.push(element)
-    end
+    if element.is_a?(Integer) then filtered_list.push(element) end
   end
-
   return filtered_list
 end
