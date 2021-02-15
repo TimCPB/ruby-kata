3bb1ef5ee441f33e676efbf2688ba291120d3f8e
Tim <timcpb@hotmail.com>
2021-02-15 16:56:45 +0000

    red test - adds a word to the dictionary

--- lib/word_dictionary.rb
+++ lib/word_dictionary.rb
@@ -1,9 +1,9 @@
-class WordDictionary
+# frozen_string_literal: true
 
+class WordDictionary
   attr_reader :dictionary
 
   def initialize
     @dictionary = []
   end
-
 end
--- spec/word_dictionary_spec.rb
+++ spec/word_dictionary_spec.rb
@@ -1,17 +1,22 @@
+# frozen_string_literal: true
+
 require 'word_dictionary'
 
 describe "word dictionary" do
-
   before(:each) do
     @wd = WordDictionary.new
   end
 
-  it "initializes with an empty dictionary" do
+  it 'initializes with an empty dictionary' do
     expect(@wd.dictionary).to eq([])
   end
 
-  # describe "#add_word" do
-  #   it "adds a word to the dictionary"
-  # end
-
+  describe '#add_word' do
+    it 'adds a word to the dictionary' do
+      @wd.add_word('cod')
+      @wd.add_word('code')
+      @wd.add_word('codewars')
+      expect(@wd.dictionary).to include('cod', 'code', 'codewars')
+    end
+  end
 end
